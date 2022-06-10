89b27d9b2d4eb401db5a49104c3bb3cd4c1245d9
Gino Stoian <ginostoian@gmail.com>
2022-06-11 02:07:59 +0300

    set up express server

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,12 @@
-console.log('Server..')
+const express = require('express')
+const dotenv = require('dotenv').config()
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send('Hello')
+})
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}...`))
